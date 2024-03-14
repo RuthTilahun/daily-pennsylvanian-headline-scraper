207d77e45d211195386bf6760f15b8965225e70a
Ruth Tilahun <128506408+RuthTilahun@users.noreply.github.com>
2024-03-14 13:55:16 -0400

    Update script.py

--- script.py
+++ script.py
@@ -27,7 +27,7 @@ def scrape_data_point():
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
         target_elements = soup.find_all("a", class_="frontpage-link")
-        data_point = " ".join([elem.text for elem in target_elements])
+        data_point = "\n".join([elem.text for elem in target_elements])
         # data_point = "" if target_element is None else target_element.text
         loguru.logger.info(f"Data point: {data_point}")
         return data_point
